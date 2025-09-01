c6d2ac07de0936fb6ac216c6abe8dbebce091015
noreplydev <contact.cristiansanchez@gmail.com>
2025-09-01 02:44:42 +0200

    add animation to the whole page

--- components/root.tsx
+++ components/root.tsx
@@ -11,17 +11,17 @@ export function Root({ mdxSource }: any) {
     "l": 1,
   }
 
-  return <main className="relative h-full w-full flex flex-col justify-start items-center z-0 gap-14 overflow-y-scroll pt-40 pb-20">
+  return <main className="relative h-full w-full flex flex-col justify-start items-center z-0 gap-5 overflow-y-scroll pt-40 pb-20">
     <div className="relative flex flex-col h-fit w-full justify-start items-center">
-      <img src="/self.png" className="opacity-0 w-[100px] h-[100px] animate-[appear_0.4s_ease-in-out_forwards] [animation-delay:0.5s]" />
+      <img src="/self.png" className="opacity-0 w-[100px] h-[100px] animate-[appear-logo_0.4s_ease-in-out_forwards] [animation-delay:0.5s]" />
+    </div>
+    <div className="w-full flex flex-col opacity-0 px-4 md:px-56  animate-[appear_0.4s_ease-in-out_forwards] [animation-delay:0.7s]">
       <Badge
         bg="#ffa719"
-        style={{ color: "#7a500d", fontSize: "16px", fontWeight: "200", marginTop: "20px" }}
+        style={{ color: "#7a500d", width: "fit-content", fontSize: "16px", fontWeight: "200", marginBlock: "20px", alignSelf: "center" }}
         translucent={true}>
         implemented research
       </Badge>
-    </div>
-    <div className="w-full flex flex-col px-4 md:px-56">
       <MDXRemote {...mdxSource} components={MDXComponents} />
     </div>
   </main >
